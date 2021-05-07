1842d460a520ac7feaa36bbc48e7cf6100ab92e2
P@tB <69919456+Code-Me-live@users.noreply.github.com>
2021-05-07 22:09:22 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -20,7 +20,7 @@ function handleTabletChange(e) {
 }
 /********************************************************/
 
-mediaQuery.addListener(handleTabletChange);
+
 
 handleTabletChange(mediaQuery);
 
