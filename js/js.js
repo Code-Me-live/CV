4e8b36caabc4822227342aa4605a796ed9a1b499
P@tB <69919456+Code-Me-live@users.noreply.github.com>
2021-05-30 01:46:40 +0200

    Add files via upload
    Version Mobile sans console.log

--- js/js.js
+++ js/js.js
@@ -1,5 +1,6 @@
 
 function menuDeroulant(id) {
+    
     let niv1 = document.getElementById(id);
     let niv2 = niv1.children; //enfantS
     let titreId = niv2.item(0).id; //id de l'enfant n°1
@@ -9,7 +10,7 @@ function menuDeroulant(id) {
     let niv5 = niv4.id; // id de l'enfant ul
     let aff = document.getElementById(niv5); //récup du dernier élément par l'id
     let test = aff.style.display;
-    console.log(test);
+
     if (aff.style.display === 'none' || test===""){
         aff.style.display ='block';
         titre.style.borderBottomLeftRadius = '0px';
