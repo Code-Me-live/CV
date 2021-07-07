3c2f20379729bec29ce5ad1844fe8d01a110423a
P@tB <69919456+PatB-Dev@users.noreply.github.com>
2021-07-07 02:00:21 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,24 +1,24 @@
 if(window.matchMedia("(max-width: 770px)").matches){
-    function menuDeroulant(id) {
-    let niv1 = document.getElementById(id);
-    let niv2 = niv1.children; //enfantS
-    let titreId = niv2.item(0).id; //id de l'enfant n°1
-    let titre = document.getElementById(titreId);
-    let niv3 = niv2.item(1).id; //id de l'enfant n°2
-    let niv4 = document.getElementById(niv3);
-    let niv5 = niv4.id; // id de l'enfant ul
-    let aff = document.getElementById(niv5); //récup du dernier élément par l'id
-    let test = aff.style.display;
+        function menuDeroulant(id) {
+        let niv1 = document.getElementById(id);
+        let niv2 = niv1.children; //enfantS
+        let titreId = niv2.item(0).id; //id de l'enfant n°1
+        let titre = document.getElementById(titreId);
+        let niv3 = niv2.item(1).id; //id de l'enfant n°2
+        let niv4 = document.getElementById(niv3);
+        let niv5 = niv4.id; // id de l'enfant ul
+        let aff = document.getElementById(niv5); //récup du dernier élément par l'id
+        let display = aff.style.display;
 
-    if (aff.style.display === 'none' || test===""){
-        aff.style.display ='block';
-        titre.style.borderBottomLeftRadius = '0px';
-        titre.style.borderBottomRightRadius = '0px';
-    } else {
-        aff.style.display ='none';
-        titre.style.borderBottomLeftRadius = '25px';
-        titre.style.borderBottomRightRadius = '25px';
-    }
+        if (aff.style.display === 'none' || display ===""){
+            aff.style.display ='block';
+            titre.style.borderBottomLeftRadius = '0px';
+            titre.style.borderBottomRightRadius = '0px';
+        } else {
+            aff.style.display ='none';
+            titre.style.borderBottomLeftRadius = '25px';
+            titre.style.borderBottomRightRadius = '25px';
+        }
     }
 }
 
@@ -39,12 +39,15 @@ if(window.matchMedia("(min-width: 770px)").matches){
         let niv4 = document.getElementById(niv3);
         let niv5 = niv4.id; // id de l'enfant ul
         let aff = document.getElementById(niv5); //récup du dernier élément par l'id
-        let test = aff.style.display;
+        let display = aff.style.display;
 
-        if (aff.style.display === 'none' || test===""){
+        if (aff.style.display === 'none' || display === ""){
             aff.style.display ='block';
             titre.style.borderBottomLeftRadius = '0px';
             titre.style.borderBottomRightRadius = '0px';
+            if(niv1.id){
+                niv1.style.zIndex ='2';
+            }
 
         } else {
             aff.style.display ='none';
